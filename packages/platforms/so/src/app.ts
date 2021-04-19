ff3c68e5f5cae1aa744083a88bc95ce32625ed7f
jbis9051 <josh9051@gmail.com>
2021-04-19 07:43:50 -0400

    Fix welcome link

--- packages/platforms/so/src/app.ts
+++ packages/platforms/so/src/app.ts
@@ -47,7 +47,7 @@ const bot = new Bot('so', {
             '193540':
                 "Welcome to the Test My Bot chat. Feel free to test @JamesBot using `|| command args` syntax. You can also discuss and ask questions about bot creation.  StackOverflow and StackOverflow Chat rules apply. Be nice and don't ask to ask, just ask. Chat API Documentation can be found [here](https://github.com/jbis9051/JamesSOBot/blob/master/docs/CHAT_API.md)",
             '17':
-                "Welcome to the JavaScript chat! Please review the [room rules](https://rlemon.github.com/so-chat-javascript-rules/). If you have a question, just post it, and if anyone's free and interested they'll help. If you want to report an abusive user or a problem in this room, visit our [meta](https://github.com/JavaScriptRoom/culture/).",
+                "Welcome to the JavaScript chat! Please review the [room rules](https://javascriptroom.github.io/rules/). If you have a question, just post it, and if anyone's free and interested they'll help. If you want to report an abusive user or a problem in this room, visit our [meta](https://github.com/JavaScriptRoom/culture/).",
         },
         code_check: ['17', '193540'],
         auto_command: ['17', '193540', '1'],
