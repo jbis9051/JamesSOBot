89a12e605d18ebbcd5bb0ce32a73f5ca33c313c8
Parking Master <parkingmaster@email.com>
2023-08-01 23:01:50 -0400

    Create request.ts

---
+++ packages/plugins/src/plugins/request.ts
@@ -0,0 +1,38 @@
+import { PermissionType, PluginFunction } from '@chatbot/bot';
+import fetch from 'node-fetch';
+
+import { PermissionType, PluginFunction } from '@chatbot/bot';
+import fetch from 'node-fetch';
+
+export const calc: PluginFunction = (bot) => {
+    bot.addCommand({
+        name: 'request',
+        args: [],
+        description: 'Sends an HTTP post request and gives a response',
+        shortcuts: ['request', 'fetch', 'http'],
+        examples: ['||request https://httpbin.org/post POST "arg1: value, arg2: value"', '||request https://github.com/ GET'],
+        ignore: false,
+        permissions: [PermissionType.ALL],
+        cb: async (msg, client) => {
+            let params = {
+              method: msg.args[1],
+              headers: {
+                "Content-Type": "application/json"
+              }
+            };
+            if (msg.args[1] !== 'GET') {
+                let body = eval('({' + msg.args[2] + '})');
+                params.body = JSON.stringify(body);
+            }
+            let then = Date.now();
+            const response = await fetch(msg.args[0], params);
+            let now = Date.now();
+            let totalTimeMS = now - then;
+            if (!(response.status === 200 || response.status === 400)) {
+                client.hardReply('The destination ' + msg.args[0] + ' could not be reached (ERR ' + response.status + ')', msg);
+            } else {
+                client.hardReply(`[${totalTimeMS}ms] ${msg.args[1]} request responded: "${await response.text()}" response content: ${JSON.stringify(response)}`, msg);
+            }
+        },
+    });
+};
