8ec4538850af8e1964e8575b45007371c6aba961
Parking Master <parkingmaster@email.com>
2023-08-01 23:22:48 -0400

    Update request.ts

--- packages/plugins/src/plugins/request.ts
+++ packages/plugins/src/plugins/request.ts
@@ -7,7 +7,7 @@ export const calc: PluginFunction = (bot) => {
         args: [],
         description: 'Sends an HTTP post request and gives a response',
         shortcuts: ['request', 'fetch', 'http'],
-        examples: ['||request https://httpbin.org/post POST "arg1: value, arg2: value"', '||request https://github.com/ GET'],
+        examples: ['||request [url] [method] [body]', '||request https://httpbin.org/post POST "arg1: value, arg2: value"', '||request https://github.com/ GET'],
         ignore: false,
         permissions: [PermissionType.ALL],
         cb: async (msg, client) => {
@@ -17,6 +17,8 @@ export const calc: PluginFunction = (bot) => {
                 "Content-Type": "application/json"
               }
             };
+            if (!(msg.args[0] && msg.args[1])) return client.hardReply('Error: The "url" and "method" arguments are required', msg);
+            if (!msg.args[2]) msg.args[2] = "";
             if (msg.args[1] !== 'GET') {
                 let body = eval('({' + msg.args[2] + '})');
                 params.body = JSON.stringify(body);
@@ -25,7 +27,7 @@ export const calc: PluginFunction = (bot) => {
             const response = await fetch(msg.args[0], params);
             let now = Date.now();
             let totalTimeMS = now - then;
-            client.hardReply(`[${totalTimeMS}ms] ${msg.args[1]} request responded: "${await response.text()}" response content: ${JSON.stringify(response)}`, msg);
+            client.hardReply(`[${totalTimeMS}ms] ${msg.args[1]} request responded: "${await response.text()}" - response content: ${JSON.stringify(response)}`, msg);
         },
     });
 };
