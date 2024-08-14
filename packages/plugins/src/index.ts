c7e863d887fe60493a183e9d42f2d6ce27eb15d2
jbis9051 <josh9051@gmail.com>
2024-08-14 18:54:27 -0400

    update node again 5

--- packages/plugins/src/index.ts
+++ packages/plugins/src/index.ts
@@ -27,7 +27,6 @@ export * from './plugins/mdn';
 export * from './plugins/obama';
 export * from './plugins/random';
 export * from './plugins/rules';
-export * from '@chatbot/so/src/plugins/stat';
 export * from './plugins/STOP';
 export * from './plugins/timeout';
 export * from './plugins/timer';
